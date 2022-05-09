f0fd158bd3f9b4467edab1f8165d80ad90a3e95a
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-05-09 13:57:18 -0400

    app functionality of loading noted from db.json is now functional

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //server.js for setting up express server
 const express = require('express');
 const path = require('path');
-const PORT = process.env.port || 3001;
+const {db} = require('./db/db.json')
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +12,15 @@ app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    let results = db;
+    res.json(results);
 })
 
 app.listen(PORT, () => {console.log(`API server now on port ${PORT}!`);}); 
