3dae3d76712a8beb43d4ae44fd528ce8f84c9398
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-05-09 14:12:54 -0400

    added delete note ability to app

--- server.js
+++ server.js
@@ -12,15 +12,30 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+function findById(id, noteList) {
+    return noteList.filter(note => note.id === id)[0];
+}
+
 function addNewNote(body, noteList) {
     let newNote = body;
-    db.push(newNote);
+    noteList.push(newNote);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
         JSON.stringify({db: noteList}, null, 2)
     );
 }
-
+function removeNote (id, noteList) {
+    const removeThisNote = findById(id, noteList);
+    for (let i = 0; i<noteList.length; i++){
+        if (noteList[i].id === removeThisNote.id) {
+            noteList.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify({db: noteList}, null, 2)
+            );
+        }
+    };
+}
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 })
@@ -37,10 +52,15 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
     //generate a new id 
     req.body.id = uniqid();
-    console.log(req.body);
+
     res.json(req.body);
     addNewNote(req.body, db);
 
 })
+
+app.delete('/api/notes/:id', (req, res) => {
+    removeNote(req.params.id, db);
+    res.json(req.body);
+});
 
 app.listen(PORT, () => {console.log(`API server now on port ${PORT}!`);}); 
